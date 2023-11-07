a2707e1c93b201ab019c8c1d32757629c673cf06
Oliver Fabián Stetcu Stepanov <100787553+ZiwiiDev@users.noreply.github.com>
2023-11-07 16:34:58 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/components/member/member.html',
+        controller: 'MemberController',
+        controllerAs: 'memberCtrl'
+    };
+}
